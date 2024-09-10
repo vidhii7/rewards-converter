5b52844bda91b737d94164def82d1fec118a2599
Vidhi Jain <74243293+vidhii7@users.noreply.github.com>
2024-09-10 11:26:26 +0530

    New class added

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,21 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+    private static final double MILES_TO_CASH_RATE = 0.0035;
+
+    // Constructor that accepts a cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / MILES_TO_CASH_RATE;
+    }
+
+    // Method to get cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Method to get miles value
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
